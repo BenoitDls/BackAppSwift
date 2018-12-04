18d9919dc8551332431782072e2a27d7a845b215
Dillies Benoît <benoit.dillies@ynov.com>
2018-12-04 17:22:55 +0100

    update model thing

--- models/Thing.js
+++ models/Thing.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const ThingSchema = mongoose.Schema({
     name: {type: String, required: true},
     description: {type: String, required: false},
-    type: {type: String, required: true},
-    warning_low: {type: Number, required: true},
-    warning_high: {type: Number, required: true},
-    measure_unit: {type: String, required: true},
+    type: {type: String, required: false},
+    warning_low: {type: Number, required: false},
+    warning_high: {type: Number, required: false},
+    measure_unit: {type: String, required: false},
     value: {type: String, required: true},
     date: {type: Date, required: true}
 })
